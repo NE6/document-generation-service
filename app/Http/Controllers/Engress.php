2dca87827bec88b08d179f99712b22ad8402f2d2
Frazer Lillie <frazer.c.lillie@gmail.com>
2022-06-28 15:40:16 +0100

    Store as S3

--- app/Http/Controllers/Engress.php
+++ app/Http/Controllers/Engress.php
@@ -4,12 +4,40 @@ namespace App\Http\Controllers;
 
 use App\DataTransferObjects\Document as DocumentDataTransferObject;
 use App\Http\Requests\Document;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Wnx\SidecarBrowsershot\BrowsershotLambda;
 
 class Engress extends Controller
 {
+    /**
+     * Responsible for making sidecar request, and sending contents to render.
+     * Will return formatted response.
+     *
+     * @param Document $request
+     * @return JsonResponse
+     */
     public function consumeDocumentRequest(Document $request)
     {
         $document = new DocumentDataTransferObject(...$request->all());
-        dd($document);
+        $randomlyGeneratedFilename = Str::random();
+
+        try {
+
+            $generatedDocument = BrowsershotLambda::html($document->contents)->pdf();
+            Storage::disk('s3')->put("$randomlyGeneratedFilename.pdf", $generatedDocument);
+
+            return response()->json([
+                'id' => $document->id,
+                'name' => $document->name,
+                'filename' => $randomlyGeneratedFilename
+            ]);
+
+        } catch (\Exception $e) {
+            return response(500)->json([
+                'error' => $e->getMessage()
+            ]);
+        }
     }
 }
